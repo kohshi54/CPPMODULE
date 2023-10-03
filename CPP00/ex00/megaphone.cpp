73e7e87aaeda0645cf664f20c05649e7ce715479
kohshi54 <kohshi54.yam@gmail.com>
2023-10-03 23:46:27 +0900

    megaphone.

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cctype>
+
+void	toUpperCase(std::string& input) {
+    for (size_t i = 0; i < input.size(); ++i)
+	{
+        input[i] = std::toupper(static_cast<unsigned char>(input[i]));
+    }
+}
+
+int	main(int argc, char *argv[])
+{
+    if (argc < 2)
+	{
+        std::cerr << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (1);
+    }
+	for (int i = 1; i < argc; ++i)
+	{
+	    std::string inputString = argv[i];
+		toUpperCase(inputString);
+		std::cout << inputString;
+	}
+	std::cout << std::endl;
+    return (0);
+}
