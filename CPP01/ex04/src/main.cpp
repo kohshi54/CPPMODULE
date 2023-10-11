5ebcc539584c5c875c04237bacc27daf7051dba9
kohshi54 <kohshi54.yam@gmail.com>
2023-10-11 15:42:40 +0900

    avoid segfault.

--- CPP01/ex04/src/main.cpp
+++ CPP01/ex04/src/main.cpp
@@ -3,17 +3,17 @@
 
 int main(int argc, char *argv[])
 {
-	MyFstream infile;
-	MyFstream newfile;
-	std::string filename = argv[1];
-	std::string s1 = argv[2];
-	std::string s2 = argv[3];
-
 	if (argc != 4)
 	{
 		std::cerr << "wrong number of arguments" << std::endl;
 		return (1);
 	}
+
+	MyFstream infile;
+	MyFstream newfile;
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
 
 	if (infile.openFile(filename, std::ios::in))
 	{
