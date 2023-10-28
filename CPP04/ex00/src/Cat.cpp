ec99adf9c9d39b5187c5ff946218fea06dda756f
kohshi54 <kohshi54.yam@gmail.com>
2023-10-28 15:11:45 +0900

    Polymorphism.

---
+++ CPP04/ex00/src/Cat.cpp
@@ -0,0 +1,29 @@
+#include "Cat.hpp"
+
+Cat::Cat() : Animal("Cat")
+{
+    std::cout << "Cat constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat &other)
+{
+    std::cout << "Cat copy constructor called" << std::endl;
+    *this = other;
+}
+
+Cat &Cat::operator=(const Cat &rhs)
+{
+    std::cout << "Cat copy assignment operator called" << std::endl;
+    this->_type = rhs._type;
+    return *this;
+}
+
+Cat::~Cat()
+{
+    std::cout << "Cat destructor called" << std::endl;
+}
+
+void Cat::makeSound() const
+{
+    std::cout << "meow meow" << std::endl;
+}
---
+++ CPP04/ex00/src/Dog.cpp
@@ -0,0 +1,29 @@
+#include "Dog.hpp"
+
+Dog::Dog() : Animal("Dog")
+{
+    std::cout << "Dog constructor called" << std::endl;
+}
+
+Dog::Dog(const Dog &other)
+{
+    std::cout << "Dog copy constructor called" << std::endl;
+    *this = other;
+}
+
+Dog &Dog::operator=(const Dog &rhs)
+{
+    std::cout << "Dog copy assignment operator called" << std::endl;
+    this->_type = rhs._type;
+    return *this;
+}
+
+Dog::~Dog()
+{
+    std::cout << "Dog destructor called" << std::endl;
+}
+
+void Dog::makeSound() const
+{
+    std::cout << "bark bark" << std::endl;
+}
