6a65cb92ed40734a90cc11e688341b343128ecff
kohshi54 <kohshi54.yam@gmail.com>
2023-11-06 17:46:58 +0900

    init ex01.

---
+++ CPP04/ex01/include/Dog.hpp
@@ -0,0 +1,19 @@
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include <iostream>
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+class Dog : public Animal
+{
+    public:
+        Dog();
+        Dog(const Dog &other);
+        Dog &operator=(const Dog &rhs);
+        ~Dog();
+
+        void makeSound() const;
+};
+
+#endif
