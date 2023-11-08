6a018489abd3d4c38f5db688083d37999cbefb0d
kohshi54 <kohshi54.yam@gmail.com>
2023-11-08 14:58:31 +0900

    add AMateria class.

---
+++ CPP04/ex03/src/AMateria.cpp
@@ -0,0 +1,40 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria() : _type("")
+{
+    std::cout << "AMaetria default constructor called" << std::endl;
+}
+
+AMateria::AMateria(std::string const &type) : _type(type)
+{
+    std::cout << "AMaetria parameterized constructor called" << std::endl;
+}
+
+AMateria::AMateria(const AMateria &other) : _type(other.getType())
+{
+    std::cout << "AMaetria copy constructor called" << std::endl;
+}
+
+AMateria &AMateria::operator=(const AMateria &rhs)
+{
+    if (this != &rhs)
+    {
+        this->_type = rhs._type;
+    }
+    return *this;
+}
+
+AMateria::~AMateria()
+{
+    std::cout << "AMateria destructor called" << std::endl;
+}
+
+std::string AMateria::const & getType() const
+{
+    return this->_type;
+}
+
+void AMateria::use(ICharacter& target)
+{
+    std::cout << "AMateria use function called" << std::endl;
+}
