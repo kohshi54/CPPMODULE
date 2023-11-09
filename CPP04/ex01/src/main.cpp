7ffc54c20cad7329fffc9e597e90770db00fd49e
Kohshi Yamaguchi <kyamaguc@c1r16s6.42tokyo.jp>
2023-11-09 20:11:12 +0900

    modify test.

--- CPP04/ex01/src/main.cpp
+++ CPP04/ex01/src/main.cpp
@@ -3,34 +3,44 @@
 
 int main()
 {
-	const Animal *array[4] = {new Dog(), new Dog(), new Cat(), new Cat()};
+	{
+		const Animal *array[4] = {new Dog(), new Dog(), new Cat(), new Cat()};
+		
+		for(int i = 0; i < 4; ++i)
+		{
+			array[i]->makeSound();
+			delete array[i];
+		}
+	}
 
-	Animal test;
-	Dog test2;
+	std::cout << "======================" << std::endl;
 
-	std::cout << test.getType() << ":" << &(test.getType()) << std::endl;
-	std::cout << test2.getType() << ":" << &(test2.getType()) << std::endl;
+	{
+		Animal test;
+		Dog test2;
 
-	test = test2;
+		std::cout << test.getType() << ":" << &(test.getType()) << std::endl;
+		std::cout << test2.getType() << ":" << &(test2.getType()) << std::endl;
 
-	std::cout << test.getType() << ":" << &(test.getType()) << std::endl;
-	std::cout << test2.getType() << ":" << &(test2.getType()) << std::endl;
+		test = test2;
 
-	Dog t1;
-	Dog t2;
+		std::cout << test.getType() << ":" << &(test.getType()) << std::endl;
+		std::cout << test2.getType() << ":" << &(test2.getType()) << std::endl;
+	}
 
-	// std::cout << "t1:" << t1.getBrain() << std::endl;
-	// std::cout << "t2:" << t2.getBrain() << std::endl;
+	std::cout << "======================" << std::endl;
 
-	t1 = t2;
+	{
+		Dog t1;
+		Dog t2;
 
-	std::cout << "t1:" << t1.getBrain() << std::endl;
-	std::cout << "t2:" << t2.getBrain() << std::endl;
-	
-    for(int i = 0; i < 4; ++i)
-    {
-		array[i]->makeSound();
-		delete array[i];
+		// std::cout << "t1:" << t1.getBrain() << std::endl;
+		// std::cout << "t2:" << t2.getBrain() << std::endl;
+
+		t1 = t2;
+
+		std::cout << "t1:" << t1.getBrain() << std::endl;
+		std::cout << "t2:" << t2.getBrain() << std::endl;
 	}
 
     return 0;
