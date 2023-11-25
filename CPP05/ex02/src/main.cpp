689df09c1468f40f87243ef423c7a07c35e39b8e
kohshi54 <kohshi54.yam@gmail.com>
2023-11-25 23:19:08 +0900

    add test.

--- CPP05/ex02/src/main.cpp
+++ CPP05/ex02/src/main.cpp
@@ -1,7 +1,36 @@
 #include "Bureaucrat.hpp"
-#include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include <iostream>
 
-int main(void)
-{
-   return 0;
+int main() {
+    try {
+        // Bureaucratの作成
+        Bureaucrat bob("Bob", 50);
+        Bureaucrat alice("Alice", 2);
+
+        std::cout << bob << std::endl;
+        std::cout << alice << std::endl;
+
+        // フォームの作成
+        ShrubberyCreationForm shrubberyForm("Home");
+        RobotomyRequestForm robotomyForm("Bob");
+        PresidentialPardonForm pardonForm("Alice");
+
+        // フォームの署名
+        bob.signForm(shrubberyForm);
+        alice.signForm(robotomyForm);
+        alice.signForm(pardonForm);
+
+        // フォームの実行
+        bob.executeForm(shrubberyForm);
+        alice.executeForm(robotomyForm);
+        alice.executeForm(pardonForm);
+
+    } catch (std::exception &e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
