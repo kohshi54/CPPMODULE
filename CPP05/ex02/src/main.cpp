f7f60c9fd9e67c325cf97954c34081f821ac852c
kohshi54 <kohshi54.yam@gmail.com>
2023-11-25 23:25:41 +0900

    add failure test.

--- CPP05/ex02/src/main.cpp
+++ CPP05/ex02/src/main.cpp
@@ -4,33 +4,61 @@
 #include "PresidentialPardonForm.hpp"
 #include <iostream>
 
-int main() {
-    try {
-        // Bureaucratの作成
-        Bureaucrat bob("Bob", 50);
-        Bureaucrat alice("Alice", 2);
+int main()
+{
+   {
+      // should pass most, robotomy may fail.
+      try {
+         Bureaucrat bob("Bob", 50);
+         Bureaucrat alice("Alice", 2);
 
-        std::cout << bob << std::endl;
-        std::cout << alice << std::endl;
+         std::cout << bob << std::endl;
+         std::cout << alice << std::endl;
 
-        // フォームの作成
-        ShrubberyCreationForm shrubberyForm("Home");
-        RobotomyRequestForm robotomyForm("Bob");
-        PresidentialPardonForm pardonForm("Alice");
+         ShrubberyCreationForm shrubberyForm("Home");
+         RobotomyRequestForm robotomyForm("Bob");
+         PresidentialPardonForm pardonForm("Alice");
 
-        // フォームの署名
-        bob.signForm(shrubberyForm);
-        alice.signForm(robotomyForm);
-        alice.signForm(pardonForm);
+         bob.signForm(shrubberyForm);
+         alice.signForm(robotomyForm);
+         alice.signForm(pardonForm);
 
-        // フォームの実行
-        bob.executeForm(shrubberyForm);
-        alice.executeForm(robotomyForm);
-        alice.executeForm(pardonForm);
+         bob.executeForm(shrubberyForm);
+         alice.executeForm(robotomyForm);
+         alice.executeForm(pardonForm);
+      }
+      catch (std::exception &e)
+      {
+         std::cerr << "Exception: " << e.what() << std::endl;
+      }
+   }
+   std::cout << "========================" << std::endl;
+   {
+      // should fail
+      try {
+         Bureaucrat bob("Bob", 150);
+         Bureaucrat alice("Alice", 150);
 
-    } catch (std::exception &e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
+         std::cout << bob << std::endl;
+         std::cout << alice << std::endl;
 
-    return 0;
+         ShrubberyCreationForm shrubberyForm("Home");
+         RobotomyRequestForm robotomyForm("Bob");
+         PresidentialPardonForm pardonForm("Alice");
+
+         bob.signForm(shrubberyForm);
+         alice.signForm(robotomyForm);
+         alice.signForm(pardonForm);
+
+         bob.executeForm(shrubberyForm);
+         alice.executeForm(robotomyForm);
+         alice.executeForm(pardonForm);
+      }
+      catch (std::exception &e)
+      {
+         std::cerr << "Exception: " << e.what() << std::endl;
+      }
+   }
+
+   return 0;
 }
