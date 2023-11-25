f0801c655f918d07363e6e12ead24c8cb53cd44f
kohshi54 <kohshi54.yam@gmail.com>
2023-11-25 22:17:56 +0900

    modify AForm class.

--- CPP05/ex02/include/AForm.hpp
+++ CPP05/ex02/include/AForm.hpp
@@ -8,8 +8,7 @@ class Bureaucrat;
 class AForm
 {
     public:
-        AForm();
-        AForm(const std::string& name, bool isSigned, int requiredGrade);
+        AForm(const std::string& name, int requiredGrade);
         AForm(const AForm& other);
         virtual ~AForm();
 
@@ -29,17 +28,20 @@ class AForm
         int getRequiredGrade() const;
         bool getIsSigned() const;
         void validateRequiredGrade();
-        virtual void validateRequiredGradeToSign(const Bureaucrat& b) = 0;
-        virtual void validateRequiredGradeToExecute(const Bureaucrat& b) = 0;
 
         void beSigned(const Bureaucrat& b);
-        void execute(Bureaucrat const & executor) const;
+        virtual void execute(Bureaucrat const & executor) const = 0;
 
     private:
+        AForm();
         AForm& operator=(const AForm& rhs); // privateで未定義にしておくことで使えなくする。
+
         const std::string _name;
         bool _isSigned;
         const int _requiredGrade;
+
+        static const int highestGrade = 1;
+        static const int lowestGrade = 150;
 };
 
 std::ostream& operator<<(std::ostream& os, const AForm& b);
--- CPP05/ex02/src/AForm.cpp
+++ CPP05/ex02/src/AForm.cpp
@@ -1,14 +1,17 @@
 #include "AForm.hpp"
 
-AForm::AForm() : _name(""), _isSigned(0), _requiredGrade(1)
+/*
+// 抽象クラスだしrequiredGrade設定は必ずしそうなので引数なしデフォルトコンストラクタは要らなさそう。
+AForm::AForm() : _name(""), _isSigned(false), _requiredGrade(1)
 {
 #ifdef DEBUG
     std::cout << GREEN << "AForm default constructor called" << RESET << std::endl;
 #endif
     validateRequiredGrade();
 }
+*/
 
-AForm::AForm(const std::string& name, bool isSigned, int requiredGrade) : _name(name), _isSigned(isSigned), _requiredGrade(requiredGrade)
+AForm::AForm(const std::string& name, int requiredGrade) : _name(name), _isSigned(false), _requiredGrade(requiredGrade)
 {
 #ifdef DEBUG
     std::cout << GREEN << "AForm parameterized construcotr called" << RESET << std::endl;
@@ -24,6 +27,7 @@ AForm::AForm(const AForm& other) : _name(other.getName()), _isSigned(getIsSigned
 }
 
 /*
+// const属性を持つので後から代入はできないものとする。
 AForm& AForm::operator=(const AForm& rhs)
 {
     if (this != &rhs)
@@ -50,14 +54,14 @@ void AForm::validateRequiredGrade()
 #ifdef DEBUG
     std::cout << BLUE << "AForm validateGrade() called" << RESET << std::endl;
 #endif
-    if (this->_requiredGrade < 1)
+    if (this->_requiredGrade < highestGrade)
     {
 #ifdef DEBUG
     std::cout << MAGENTA << "AForm GradeTooHighException occurred" << RESET << std::endl;
 #endif
         throw AForm::GradeTooHighException();
     }
-    else if (this->_requiredGrade > 150)
+    else if (this->_requiredGrade > lowestGrade)
     {
 #ifdef DEBUG
     std::cout << MAGENTA << "AForm GradeTooLowException occurred" << RESET << std::endl;
@@ -92,7 +96,6 @@ void AForm::beSigned(const Bureaucrat& b)
         throw AForm::GradeTooLowException();
 }
 
-
 std::ostream& operator<<(std::ostream& os, const AForm& f)
 {
     os << "AForm name: " << f.getName() << ", signedState: " << f.getIsSigned() << ", requiredGrade: " << f.getRequiredGrade();
@@ -113,10 +116,4 @@ const char *AForm::GradeTooLowException::what() const throw()
     std::cout << MAGENTA << "AForm GradeTooLowExecption::what() called" << RESET << std::endl;
 #endif
     return "grade too low";
-}
-
-void AForm::execute(Bureaucrat const & executor) const
-{
-    // AForm::validateRequiredGradeToExecute();
-    (void)executor;
 }
