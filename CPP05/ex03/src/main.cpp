ed1f4deb4b5ef20f6772ced7dab2ff8312f9eba4
kohshi54 <kohshi54.yam@gmail.com>
2023-11-26 21:52:00 +0900

    add test.

--- CPP05/ex03/src/main.cpp
+++ CPP05/ex03/src/main.cpp
@@ -2,7 +2,6 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
-#include <iostream>
 #include "Intern.hpp"
 
 int main()
@@ -12,6 +11,49 @@ int main()
       AForm* rrf;
       rrf = someRandomIntern.makeForm("robotomy request", "Bender");
       (void)rrf;
+   }
+   std::cout << "============================" << std::endl;
+   {
+      Intern someRandomIntern;
+      Bureaucrat bureaucrat("Alice", 5);
+      AForm* form;
+
+      // robotomy request form
+      form = someRandomIntern.makeForm("robotomy request", "Bender");
+      if (form)
+      {
+         std::cout << *form << std::endl;
+         bureaucrat.signForm(*form);
+         bureaucrat.executeForm(*form);
+         delete form;
+      }
+
+      // shrubbery creation form
+      form = someRandomIntern.makeForm("shrubbery creation", "Home");
+      if (form)
+      {
+         std::cout << *form << std::endl;
+         bureaucrat.signForm(*form);
+         bureaucrat.executeForm(*form);
+         delete form;
+      }
+
+      // presidential pardon form
+      form = someRandomIntern.makeForm("presidential pardon", "Bob");
+      if (form)
+      {
+         std::cout << *form << std::endl;
+         bureaucrat.signForm(*form);
+         bureaucrat.executeForm(*form);
+         delete form;
+      }
+
+      // unknow form so fail
+      form = someRandomIntern.makeForm("unknown form", "Unknown");
+      if (!form)
+      {
+         std::cout << "Failed to create form: unknown form" << std::endl;
+      }
    }
    return 0;
 }
