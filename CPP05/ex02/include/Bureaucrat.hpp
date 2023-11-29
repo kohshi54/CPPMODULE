24887c2438a94f3aa87551340e909303ff8dd010
kohshi54 <kohshi54.yam@gmail.com>
2023-11-30 01:43:34 +0900

    apply changes from ex00 and ex01.

--- CPP05/ex02/include/Bureaucrat.hpp
+++ CPP05/ex02/include/Bureaucrat.hpp
@@ -27,17 +27,17 @@ class Bureaucrat
         class GradeTooHighException : public std::exception
         {
             public:
-                const char *what() const throw();
+                virtual const char *what() const throw();
         };
 
         class GradeTooLowException : public std::exception
         {
             public:
-                const char *what() const throw();
+                virtual const char *what() const throw();
         };
   
     private:
-        std::string _name;
+        const std::string _name;
         int _grade; // grade must be in range of 1(highest) to 150(lowest).
 };
 
