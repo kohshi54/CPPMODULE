12b3b28e5bc226acbde8a24a4f26500c059680db
kohshi54 <kohshi54.yam@gmail.com>
2023-12-04 17:28:15 +0900

    swap() min() max() template.

---
+++ CPP07/ex00/include/Template.hpp
@@ -0,0 +1,30 @@
+#ifndef TEMPLATE_HPP
+# define TEMPLATE_HPP
+
+# include <iostream>
+
+template <typename T>
+void swap(T& a, T& b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+	if (a < b)
+		return a;
+	return b;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	if (a <= b)
+		return b;
+	return a;
+}
+
+#endif
--- CPP07/ex00/src/main.cpp
+++ CPP07/ex00/src/main.cpp
@@ -1,12 +1,45 @@
-#include <iostream>
+#include "Template.hpp"
 
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
+}
+
+/*
 int main(int argc, char *argv[])
 {
 	(void)argc;
 	(void)argv;
+	int a = 1;
+	int b = 2;
+	swap<int>(a, b);
+	std::cout << "a=" << a << std::endl;
+	std::cout << "b=" << b << std::endl;
+	std::cout << min<int>(1, 2) << std::endl;
+	std::cout << max<int>(1, 2) << std::endl;
+
+	std::cout << min<float>(1.3f, 12.3f) << std::endl;
+
 	std::cout << " hello" << std::endl;
 	return (0);
 }
+*/
 
 #ifdef DEBUG
 __attribute__((destructor))
