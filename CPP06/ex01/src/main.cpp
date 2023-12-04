5bf0cca6b63cc4a02d5208a6316e2e1f14a36dcc
kohshi54 <kohshi54.yam@gmail.com>
2023-12-04 11:47:57 +0900

    add debug mode.

--- CPP06/ex01/src/main.cpp
+++ CPP06/ex01/src/main.cpp
@@ -8,11 +8,12 @@ int main()
 		d->name = "mike";
 		d->age = 22;
 
-		std::cout << "before: name=" << d->age << ", age=" << d->name << std::endl;
+		std::cout << "before: name=" << d->name << ", age=" << d->age << std::endl;
 		uintptr_t serialized = Serialize::serialize(d);
 		// delete d; // here, the memory address allocated before and after reinterpret_cast is same. Therefore deleting the object will create dangling pointer.
 		Data* n = Serialize::deserialize(serialized);
-		std::cout << "after: name=" << n->age << ", age=" << n->name << std::endl;
+		std::cout << "after: name=" << n->name << ", age=" << n->age << std::endl;
+		delete d;
 	}
 	std::cout << "==================" << std::endl;
 	{
@@ -21,13 +22,20 @@ int main()
 		d.name = "mike";
 		d.age = 22;
 
-		std::cout << "before: name=" << d.age << ", age=" << d.name << std::endl;
+		std::cout << "before: name=" << d.name << ", age=" << d.age << std::endl;
 		uintptr_t serialized = Serialize::serialize(&d);
 		d.name = "bob";
 		d.age = 1;
 		Data* n = Serialize::deserialize(serialized); // the address pointed at is the same, so the change will affect n as well.
-		std::cout << "after: name=" << n->age << ", age=" << n->name << std::endl;
+		std::cout << "after: name=" << n->name << ", age=" << n->age << std::endl;
 	}
-
 	return 0;
 } 
+
+#ifdef DEBUG
+	__attribute__((destructor))
+	static void end()
+	{
+		system("leaks -q serialize");
+	}
+#endif
