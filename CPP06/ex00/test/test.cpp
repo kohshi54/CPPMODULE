535cb69ecf9fa14e01199733eba94cb41aa8fc91
kohshi54 <kohshi54.yam@gmail.com>
2023-12-04 10:53:22 +0900

    isFloat test.

--- CPP06/ex00/test/test.cpp
+++ CPP06/ex00/test/test.cpp
@@ -40,7 +40,21 @@ TEST(ConverterTest, IsInt)
 	EXPECT_FALSE(ScalarConverter::isInt("b"));
 	// EXPECT_FALSE(ScalarConverter::isInt("2147483648")); // Here isInt() does not return false, but stoi throws exception when overflow so ok.
 	// EXPECT_FALSE(ScalarConverter::isInt("-2147483649")); // 
+}
 
+TEST(ConverterTest, IsFloat)
+{
+	EXPECT_TRUE(ScalarConverter::isFloat("4.2f"));
+	EXPECT_TRUE(ScalarConverter::isFloat("0f"));
+	EXPECT_TRUE(ScalarConverter::isFloat(".0f"));
+	EXPECT_TRUE(ScalarConverter::isFloat("0.0f"));
+
+	EXPECT_FALSE(ScalarConverter::isFloat("1"));
+	EXPECT_FALSE(ScalarConverter::isFloat("10"));
+	EXPECT_FALSE(ScalarConverter::isFloat("aa"));
+	EXPECT_FALSE(ScalarConverter::isFloat("AA"));
+
+	// EXPECT_TRUE(ScalarConverter::isChar("\t")); // If handle escape sequence, it can be handle correctly.
 }
 
 int main(int argc, char *argv[])
