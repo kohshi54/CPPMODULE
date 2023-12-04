a95db42c9c110f22fa7ebfdceda716f9f7d1fd17
kohshi54 <kohshi54.yam@gmail.com>
2023-12-04 09:41:05 +0900

    isChar more tests.

--- CPP06/ex00/test/test.cpp
+++ CPP06/ex00/test/test.cpp
@@ -4,11 +4,16 @@
 TEST(ConverterTest, IsChar)
 {
 	EXPECT_TRUE(ScalarConverter::isChar("a"));
-	EXPECT_TRUE(ScalarConverter::isChar("b"));
+	EXPECT_TRUE(ScalarConverter::isChar("A"));
 	EXPECT_TRUE(ScalarConverter::isChar("*"));
 	EXPECT_TRUE(ScalarConverter::isChar("+"));
 
 	EXPECT_FALSE(ScalarConverter::isChar("1"));
+	EXPECT_FALSE(ScalarConverter::isChar("10"));
+	EXPECT_FALSE(ScalarConverter::isChar("aa"));
+	EXPECT_FALSE(ScalarConverter::isChar("AA"));
+
+	// EXPECT_TRUE(ScalarConverter::isChar("\t")); // If handle escape sequence, it can be handle correctly.
 }
 
 int main(int argc, char *argv[])
