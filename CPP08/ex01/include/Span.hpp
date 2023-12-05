c00e511fa7f5ae02b24921e755ddaf2f6002863d
kohshi54 <kohshi54.yam@gmail.com>
2023-12-06 06:47:13 +0900

    Span class.

---
+++ CPP08/ex01/include/Span.hpp
@@ -0,0 +1,39 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <exception>
+#include <limits>
+
+class Span
+{
+	private:
+		std::vector<int> numbers;
+		unsigned int max_size;
+	
+	public:
+		Span();
+		Span(unsigned int N);
+		Span(const Span& other);
+		Span& operator=(const Span& rhs);
+		~Span();
+
+		void addNumber(int number);
+		
+		template <typename Iterator>
+		void addNumbers(Iterator begin, Iterator end);
+		int shortestSpan();
+		int longestSpan();
+
+		class SpanException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+#include "Span.tpp"
+
+#endif
