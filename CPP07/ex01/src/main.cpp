088f2844ba1eb67f61ebff42f3090503ba3dbb0d
kohshi54 <kohshi54.yam@gmail.com>
2023-12-05 09:09:56 +0900

    more tests.

--- CPP07/ex01/src/main.cpp
+++ CPP07/ex01/src/main.cpp
@@ -5,10 +5,31 @@ void print(int i)
 	std::cout << i << std::endl;
 }
 
+template <typename T>
+void printTemplate(T element)
+{
+	std::cout << element << std::endl;
+}
+
 int main(void)
 {
-	int array[] = {1, 2, 3, 4};
-	iter(array, 4, print);
+	{
+		int array[] = {1, 2, 3, 4};
+		std::cout << "normal function" << std::endl;
+		iter(array, 4, print);
+	}
+	std::cout << "=================" << std::endl;
+	{
+		std::cout << "function template int" << std::endl;
+		int array[] = {5, 6, 7, 8};
+		iter(array, 4, printTemplate<int>);
+	}
+	std::cout << "=================" << std::endl;
+	{
+		std::cout << "function template string" << std::endl;
+		std::string a[] = {"hello", "world"};
+		iter(a, 2, printTemplate<std::string>);		
+	}
 	return 0;
 }
 
