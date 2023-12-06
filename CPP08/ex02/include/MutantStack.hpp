307bf13e6a0f47407ecef10c562470b97cffccf3
kohshi54 <kohshi54.yam@gmail.com>
2023-12-06 09:27:53 +0900

    add iterator to stack.

---
+++ CPP08/ex02/include/MutantStack.hpp
@@ -0,0 +1,27 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+#include <deque>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack();
+		MutantStack(const MutantStack &other);
+		MutantStack &operator=(const MutantStack &other);
+		~MutantStack();
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+		iterator begin();
+		iterator end();
+		const_iterator begin() const;
+		const_iterator end() const;
+};
+
+#include "MutantStack.tpp"
+
+#endif
