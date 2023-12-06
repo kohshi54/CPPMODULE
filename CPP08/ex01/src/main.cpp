adeb984678497e26657057cb20ab51dee963f8bd
kohshi54 <kohshi54.yam@gmail.com>
2023-12-06 10:17:41 +0900

    a few more tests.

--- CPP08/ex01/src/main.cpp
+++ CPP08/ex01/src/main.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
 #include <vector>
 #include "Span.hpp"
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
-	Span sp = Span(5);
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+	{
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+	std::cout << "===============" << std::endl;
+	{
+		Span mySpan(10000);
+		std::vector<int> testNumbers(10000);
+
+		std::srand(static_cast<unsigned int>(std::time(NULL)));
+
+		for (size_t i = 0; i < testNumbers.size(); ++i)
+		{
+			testNumbers[i] = std::rand();
+		}
+
+		mySpan.addNumbers(testNumbers.begin(), testNumbers.end());
+		try
+		{
+			std::cout << "Shortest Span: " << mySpan.shortestSpan() << std::endl;
+			std::cout << "Longest Span: " << mySpan.longestSpan() << std::endl;
+		} catch (const std::exception& e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
+	}
 	return 0;
 }
