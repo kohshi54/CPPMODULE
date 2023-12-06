649e2da5a232a8364403e9705fe082bde3abcb05
kohshi54 <kohshi54.yam@gmail.com>
2023-12-06 10:26:40 +0900

    a few more test.

--- CPP08/ex02/src/main.cpp
+++ CPP08/ex02/src/main.cpp
@@ -1,58 +1,63 @@
 #include <iostream>
 #include "MutantStack.hpp"
-
-int main()
-{
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	// [...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
-		std::cout << *it << std::endl;
-		++it;
-	}
-	std::stack<int> s(mstack);
-	return 0;
-}
-
-/*
-#include <iostream>
 #include <list>
 
 int main()
 {
-    std::list<int> lst;
-    lst.push_back(5);
-    lst.push_back(17);
-    std::cout << lst.back() << std::endl; // Accessing the last element
-    lst.pop_back(); // Removing the last element
-    std::cout << lst.size() << std::endl;
-    lst.push_back(3);
-    lst.push_back(5);
-    lst.push_back(737);
-    // [...]
-    lst.push_back(0);
-    std::list<int>::iterator it = lst.begin();
-    std::list<int>::iterator ite = lst.end();
-    ++it;
-    --it;
-    while (it != ite)
-    {
-        std::cout << *it << std::endl;
-        ++it;
-    }
-    return 0;
+	{
+		std::cout << "--------MutantStack--------" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		// [...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	{
+		std::cout << "----------lsit-----------" << std::endl;
+		std::list<int> lst;
+		lst.push_back(5);
+		lst.push_back(17);
+		std::cout << lst.back() << std::endl;
+		lst.pop_back();
+		std::cout << lst.size() << std::endl;
+		lst.push_back(3);
+		lst.push_back(5);
+		lst.push_back(737);
+		// [...]
+		lst.push_back(0);
+		std::list<int>::iterator it = lst.begin();
+		std::list<int>::iterator ite = lst.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
+	return 0;
 }
-*/
+
+#ifdef DEBUG
+	__attribute__((destructor))
+	static void end()
+	{
+		system("leaks -q mutant");
+	}
+#endif
